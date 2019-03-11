98c1379c772d1eb0ded91a52cece1a6972069e3b
jungpaeng <meis1541@naver.com>
2019-03-11 16:47:42 +0900

    :sparkles: Create Page
    - Block
    - Transaction

--- src/Routes/Blocks/BlocksPresenter.tsx
+++ src/Routes/Blocks/BlocksPresenter.tsx
@@ -1,12 +1,33 @@
 import React, { FC } from 'react';
+import styled from 'styled-components';
 import { IBlock } from '../../@types/block';
+import { BlocksHeader, BlocksRow, Card } from '../../Components/Shared';
+import { getDateStrFromSeconds } from '../../utls';
 
 export interface IBlockProps {
   blocks: IBlock[];
 }
 
-const BlocksPresenter: FC<IBlockProps> = () => (
-  <h1>locks + txs</h1>
+const TableContainer = styled.div`
+  margin: 50px 0 100px;
+`;
+
+const BlocksPresenter: FC<IBlockProps> = ({ blocks }) => (
+  <TableContainer>
+    <h2>Latest Blocks</h2>
+    <Card>
+      <BlocksHeader />
+      {blocks.map(block => (
+        <BlocksRow
+          key={block.hash}
+          index={block.index}
+          hash={block.hash}
+          timeStamp={getDateStrFromSeconds(block.timeStamp)}
+          difficulty={block.difficulty}
+        />
+      ))}
+    </Card>
+  </TableContainer>
 );
 
 export default BlocksPresenter;
--- src/Routes/Transactions/TransactionsPresenter.tsx
+++ src/Routes/Transactions/TransactionsPresenter.tsx
@@ -1,12 +1,32 @@
+import sum from 'lodash.sum';
 import React, { FC } from 'react';
+import styled from 'styled-components';
 import { ITransaction } from '../../@types/block';
+import { Card, TxHeader, TxRow } from '../../Components/Shared';
 
 export interface ITransactionProps {
   transactions: ITransaction[];
 }
 
-const TransactionsPresenter: FC<ITransactionProps> = () => (
-  <h1>locks + txs</h1>
+const TableContainer = styled.div`
+  margin: 50px 0 100px;
+`;
+
+const TransactionsPresenter: FC<ITransactionProps> = ({ transactions }) => (
+  <TableContainer>
+    <h2>Latest Transactions</h2>
+    <Card>
+      <TxHeader />
+      {transactions.map(transaction => (
+        <TxRow
+          key={transaction .id}
+          id={transaction .id}
+          insOuts={`${transaction.txIns.length}/${transaction.txOuts.length}`}
+          amount={sum(transaction.txOuts.map(txOut => txOut.amount))}
+        />
+      ))}
+    </Card>
+  </TableContainer>
 );
 
 export default TransactionsPresenter;
