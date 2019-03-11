068ba59179e12c25cacd8f06d667bb9603e0a3e7
jungpaeng <meis1541@naver.com>
2019-03-11 12:17:01 +0900

    :sparkles: Create Blocks Route Component

---
+++ src/Components/Routes/Blocks/BlocksContainer.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react';
+import BlocksPresenter from './BlocksPresenter';
+
+const BlocksContainer: FC = () => (
+  <BlocksPresenter />
+);
+
+export default BlocksContainer;
---
+++ src/Components/Routes/Blocks/BlocksPresenter.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+
+const BlocksPresenter: FC = () => (
+  <h1>locks + txs</h1>
+);
+
+export default BlocksPresenter;
