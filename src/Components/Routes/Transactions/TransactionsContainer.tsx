fbfd0276e0f7835b0cf774cc83aea31975dba227
jungpaeng <meis1541@naver.com>
2019-03-11 12:18:16 +0900

    :sparkles: Create Transactions Route Component

---
+++ src/Components/Routes/Transactions/TransactionsContainer.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react';
+import TransactionsPresenter from './TransactionsPresenter';
+
+const TransactionsContainer: FC = () => (
+  <TransactionsPresenter />
+);
+
+export default TransactionsContainer;
---
+++ src/Components/Routes/Transactions/TransactionsPresenter.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+
+const TransactionsPresenter: FC = () => (
+  <h1>locks + txs</h1>
+);
+
+export default TransactionsPresenter;
