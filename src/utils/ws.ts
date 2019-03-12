c85ef7dfed3f0f059323cfebcf3880d99b249194
jungpaeng <meis1541@naver.com>
2019-03-13 01:10:03 +0900

    :sparkles: Add Socket Event Function

---
+++ src/utils/ws.ts
@@ -0,0 +1,29 @@
+import { WS_URL } from '../contants';
+import { stringToJSON } from '../utls';
+
+export const parseMessage = (message: any) => {
+  if (typeof message.data === 'string') {
+    const { data } = message;
+    const parsedMessage = stringToJSON(data);
+
+    if (parsedMessage !== null) {
+      const { type } = parsedMessage;
+
+      if (type === 'BLOCKCHAIN_RESPONSE') {
+        return parsedMessage.data;
+      }
+      return null;
+    }
+  }
+};
+
+export const addSocketEventListener = (callback: (message: any) => any) => {
+  const socket = new WebSocket(WS_URL);
+
+  socket.addEventListener('message', (message) => {
+    const parsedMessage = parseMessage(message);
+    if (parsedMessage !== null && parsedMessage !== undefined) {
+      callback(parsedMessage);
+    }
+  });
+};
--- src/utls.ts
+++ src/utls.ts
@@ -11,19 +11,3 @@ export const stringToJSON = (string: string) => {
     console.error(e);
   }
 };
-
-export const parseMessage = (message: any) => {
-  if (typeof message.data === 'string') {
-    const { data } = message;
-    const parsedMessage = stringToJSON(data);
-
-    if (parsedMessage !== null) {
-      const { type } = parsedMessage;
-
-      if (type === 'BLOCKCHAIN_RESPONSE') {
-        return parsedMessage.data;
-      }
-      return null;
-    }
-  }
-};
