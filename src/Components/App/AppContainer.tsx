7dae6c313a7910eda9be10efe444644c0ebf5b7e
jungpaeng <meis1541@naver.com>
2019-03-12 21:46:37 +0900

    :art: Refactoring App Component

--- src/Components/App/AppContainer.tsx
+++ src/Components/App/AppContainer.tsx
@@ -1,12 +1,7 @@
-import axios from 'axios';
-import flatten from 'lodash.flatten';
-import React, { Component } from 'react';
+import React, { FC } from 'react';
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
-import { IBlock, ITransaction } from '../../@types/block';
-import { API_URL, WS_URL } from '../../contants';
 import typography from '../../typography';
-import { parseMessage } from '../../utls';
 import AppPresenter from './AppPresenter';
 
 const GlobalStyle = createGlobalStyle`
@@ -17,65 +12,11 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
-interface IState {
-  blocks: IBlock[];
-  isLoading: boolean;
-  transactions: ITransaction[];
-}
-
-class App extends Component<{}, IState> {
-  public state: IState = {
-    blocks: [],
-    isLoading: true,
-    transactions: [],
-  };
-
-  public _connectToWs = () => {
-    const ws = new WebSocket(WS_URL);
-    ws.addEventListener('message', (message: any) => {
-      const parsedMessage = parseMessage(message);
-
-      if (parsedMessage !== null && parsedMessage !== undefined) {
-        this.setState((prevState: IState) => ({
-          ...prevState,
-          blocks: [...parsedMessage, ...prevState.blocks],
-          transactions: [...parsedMessage[0].data, ...prevState.transactions],
-        }));
-      }
-    });
-  }
-
-  public _getData = async() => {
-    const request = await axios.get<IBlock[]>(`${API_URL}/block`);
-    const blocks = request.data.reverse();
-    const txs = flatten(blocks.map((block: any) => block.data));
-
-    this.setState({
-      blocks,
-      isLoading: false,
-      transactions: txs,
-    });
-  }
-
-  public componentDidMount() {
-    this._connectToWs();
-    this._getData();
-  }
-
-  public render() {
-    const { blocks, isLoading, transactions } = this.state;
-
-    return (
-      <>
-        <GlobalStyle />
-        <AppPresenter
-          blocks={blocks}
-          isLoading={isLoading}
-          transactions={transactions}
-        />
-      </>
-    );
-  }
-}
+const App: FC = () => (
+  <>
+    <GlobalStyle />
+    <AppPresenter />
+  </>
+);
 
 export default App;
--- src/Components/App/AppPresenter.tsx
+++ src/Components/App/AppPresenter.tsx
@@ -1,7 +1,6 @@
 import React, { FC } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import styled from 'styled-components';
-import { IBlock, ITransaction } from '../../@types/block';
 import Blocks from '../../Routes/Blocks';
 import Home from '../../Routes/Home';
 import Transactions from '../../Routes/Transactions';
@@ -12,44 +11,27 @@ const AppContainer = styled.div`
   min-height: 100vh;
 `;
 
-interface IProps {
-  blocks: IBlock[];
-  isLoading: boolean;
-  transactions: ITransaction[];
-}
-
-const AppPresenter: FC<IProps> = ({ blocks, isLoading, transactions }) => (
+const AppPresenter: FC = () => (
   <BrowserRouter>
     <AppContainer>
       <Header>
-        {!isLoading && (
-          <Switch>
-            <Route
-              exact={true}
-              path={'/'}
-              render={() => (
-                <Home
-                  blocks={blocks.slice(0, 5)}
-                  transactions={transactions.slice(0, 5)}
-                />
-              )}
-            />
-            <Route
-              exact={true}
-              path={'/blocks'}
-              render={() => (
-                <Blocks blocks={blocks} />
-              )}
-            />
-            <Route
-              exact={true}
-              path={'/transactions'}
-              render={() => (
-                <Transactions transactions={transactions} />
-              )}
-            />
+        <Switch>
+          <Route
+            component={Home}
+            exact={true}
+            path={'/'}
+          />
+          <Route
+            component={Blocks}
+            exact={true}
+            path={'/blocks'}
+          />
+          <Route
+            component={Transactions}
+            exact={true}
+            path={'/transactions'}
+          />
           </Switch>
-        )}
       </Header>
     </AppContainer>
   </BrowserRouter>
