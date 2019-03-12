16285011ee20535a0a03496fa038d9d330d7f8b0
jungpaeng <meis1541@naver.com>
2019-03-13 00:54:52 +0900

    :art: Refactoring Transactions Page

--- src/Routes/Transactions/TransactionsContainer.tsx
+++ src/Routes/Transactions/TransactionsContainer.tsx
@@ -1,8 +1,47 @@
-import React, { FC } from 'react';
-import TransactionsPresenter, { ITransactionProps } from './TransactionsPresenter';
+import axios from 'axios';
+import React, { Component } from 'react';
+import { IPaginated, ITransaction } from '../../@types/block';
+import { API_URL } from '../../contants';
+import TransactionsPresenter, { ITransactionsData } from './TransactionsPresenter';
 
-const TransactionsContainer: FC<ITransactionProps> = (props: ITransactionProps) => (
-  <TransactionsPresenter {...props} />
-);
+type State = ITransactionsData;
 
-export default TransactionsContainer;
+class BlocksContainer extends Component<{}, State> {
+  public state: State = {
+    currentPage: 1,
+    isLoading: true,
+    total: 0,
+    totalPages: 0,
+    txs: [],
+  };
+
+  public componentDidMount() {
+    this.getBlocksData();
+  }
+
+  public getBlocksData = async () => {
+    const { data: {
+      currentPage, total, totalPages, data: txs,
+    } } = await axios.get<IPaginated<ITransaction[]>>(
+      `${API_URL}/transactions?page=${this.state.currentPage}`,
+    );
+
+    this.setState({
+      currentPage,
+      isLoading: false,
+      total,
+      totalPages,
+      txs,
+    });
+  }
+
+  public render() {
+    return (
+      <TransactionsPresenter
+        {...this.state}
+      />
+    );
+  }
+}
+
+export default BlocksContainer;
--- src/Routes/Transactions/TransactionsPresenter.tsx
+++ src/Routes/Transactions/TransactionsPresenter.tsx
@@ -1,32 +1,32 @@
-import sum from 'lodash.sum';
 import React, { FC } from 'react';
 import styled from 'styled-components';
-import { ITransaction } from '../../@types/block';
-import { Card, TxHeader, TxRow } from '../../Components/Shared';
+import { ILoading } from '../../@types/api';
+import { IPaginated, ITransaction } from '../../@types/block';
+import Table from '../../Components/Table';
+import { Omit } from '../../utils/typescript';
 
-export interface ITransactionProps {
-  transactions: ITransaction[];
+type Pagenated = Omit<IPaginated, 'data' | 'perPage'>;
+
+export interface ITransactionsData extends ILoading, Pagenated {
+  txs: ITransaction[];
 }
 
 const TableContainer = styled.div`
   margin: 50px 0 100px;
 `;
 
-const TransactionsPresenter: FC<ITransactionProps> = ({ transactions }) => (
-  <TableContainer>
-    <h2>Latest Transactions</h2>
-    <Card>
-      <TxHeader />
-      {transactions.map(transaction => (
-        <TxRow
-          key={transaction .id}
-          id={transaction .id}
-          insOuts={`${transaction.txIns.length}/${transaction.txOuts.length}`}
-          amount={sum(transaction.txOuts.map(txOut => txOut.amount))}
-        />
-      ))}
-    </Card>
-  </TableContainer>
+const TransactionsPresenter: FC<ITransactionsData> = ({
+  txs,
+  total,
+}) => (
+  <Table
+    title={`All Transactions (${total || 0})`}
+    data={txs.slice(0, 15)}
+    headers={['Amount', 'Transaction ID', 'Timestamp']}
+    selected={['amount', 'id', 'timestamp']}
+    linkPages={['transactions', 'transactions']}
+    linkParams={['id', 'id']}
+  />
 );
 
 export default TransactionsPresenter;
