bcf40acc6bf89acb13770e7c5e2779b317b4c0b9
jungpaeng <meis1541@naver.com>
2019-03-14 01:49:26 +0900

    :sparkles: Create TransactionTable Component

--- src/Components/Table/TransactionTable/TransactionTable.tsx
+++ src/Components/Table/TransactionTable/TransactionTable.tsx
@@ -1,19 +1,30 @@
 import React, { FC } from 'react';
+import { ITransaction } from '../../../@types/block';
 import { getDateStrFromSeconds } from '../../../utls';
 import Table, { ITableProps, TableCell, TableLink, TableRow } from '../Table';
 
 interface IProps extends ITableProps {
+  data: ITransaction[];
 }
 
 const TransactionTable: FC<IProps> = ({
   title,
   headers,
+  data,
 }) => (
   <Table
     title={title}
     headers={headers}
   >
-    <p>...</p>
+    {data.map(({ from, to, amount, timeStamp, id }, index) => (
+      <TableRow key={index}>
+        <TableLink to={`/addresses/${from}`}>{from}</TableLink>
+        <TableCell>{amount}</TableCell>
+        <TableLink to={`/addresses/${to}`}>{to}</TableLink>
+        <TableCell>{getDateStrFromSeconds(timeStamp)}</TableCell>
+        <TableLink to={`/transactions/${id}`}>Link</TableLink>
+      </TableRow>
+    ))}
   </Table>
 );
 
