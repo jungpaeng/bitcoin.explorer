325380a18d17d08cb4643768dd46f2421064f9dc
jungpaeng <meis1541@naver.com>
2019-03-14 22:04:09 +0900

    :lipstick: Paginated Component #1

---
+++ src/Components/Paginated/Paginated.tsx
@@ -0,0 +1,57 @@
+import React, { FC } from 'react';
+import ReactPaginate from 'react-paginate';
+import styled from 'styled-components';
+
+const PaginatedContainer = styled.div`
+  & .paginated-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0;
+
+    & li {
+      margin: 0;
+      border: 1px solid #dadada;
+      background: #fff;
+      color: #768a94;
+
+      &:not(:last-child) {
+        border-right: none;
+      }
+
+      & a {
+        cursor: pointer;
+        display: flex;
+        justify-content: center;
+        padding: 10px 10px;
+        width: 100%;
+        height: 100%;
+        user-select: none;
+      }
+
+      &.selected {
+        background: #3582b4;
+        color: #fff
+      }
+    }
+  }
+`;
+
+interface IProps {
+  pageCount: number;
+}
+
+const Paginated: FC<IProps> = ({ pageCount }) => (
+  <PaginatedContainer>
+    <ReactPaginate
+      pageCount={pageCount}
+      pageRangeDisplayed={2}
+      marginPagesDisplayed={3}
+      previousLabel="Prev"
+      nextLabel="Next"
+      containerClassName="paginated-container"
+    />
+  </PaginatedContainer>
+);
+
+export default Paginated;
---
+++ src/Components/Paginated/index.ts
@@ -0,0 +1,2 @@
+import Paginated from './Paginated';
+export default Paginated;
